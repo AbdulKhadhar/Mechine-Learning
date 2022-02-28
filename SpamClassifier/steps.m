6c2db7f5d2776f25690910c4c101a95df46fe3a6
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2022-02-28 22:48:13 +0530

    Update steps.m

--- SpamClassifier/steps.m
+++ SpamClassifier/steps.m
@@ -37,3 +37,10 @@ load('ex6data3.mat');
 
 % Plot training data
 plotData(X, y);
+
+% Try different SVM Parameters here
+[C, sigma] = dataset3Params(X, y, Xval, yval);
+
+% Train the SVM
+model = svmTrain(X, y, C, @(x1, x2)gaussianKernel(x1, x2, sigma));
+visualizeBoundary(X, y, model);
