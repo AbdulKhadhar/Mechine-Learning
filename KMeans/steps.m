60ed4c44ccedc75da8284aeb5f4d45ec1c5f98ba
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2022-03-06 20:30:50 +0530

    Update steps.m

--- KMeans/steps.m
+++ KMeans/steps.m
@@ -12,3 +12,20 @@ idx = findClosestCentroids(X, initial_centroids);
 % Compute means based on the closest centroids found in the previous part.
 centroids = computeCentroids(X, idx, K);
 fprintf('Centroids computed after initial finding of closest centroids: \n %f %f \n %f %f\n %f %f' , centroids');
+
+
+% Load an example dataset
+load('ex7data2.mat');
+% Settings for running K-Means
+max_iters = 10;
+
+
+initial_centroids = [3 3; 6 2; 8 5];
+
+% Run K-Means algorithm. The 'true' at the end tells our function to plot the progress of K-Means
+figure('visible','on'); hold on; 
+plotProgresskMeans(X, initial_centroids, initial_centroids, idx, K, 1); 
+xlabel('Press ENTER in command window to advance','FontWeight','bold','FontSize',14)
+[~, ~] = runkMeans(X, initial_centroids, max_iters, true);
+
+
